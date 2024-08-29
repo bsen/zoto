96d74bd11c17f34fd716ebee9e1ace121d97b444
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-08-29 23:45:46 +0530

    server updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,5 +14,5 @@ app.get("/", (req, res) => {
 app.use("/api", appRouter);
 app.use("/api/admin/zotoplatforms/panel", adminRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
