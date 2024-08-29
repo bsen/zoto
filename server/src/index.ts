c074c37b5f699e132c9644c3d554f295dcd195d3
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-08-30 00:27:39 +0530

    dotenv added

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import appRouter from "./routes/app";
 import adminRouter from "./routes/admin";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "server is live" });
