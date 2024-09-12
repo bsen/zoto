b8e9de5ce2967db014fae5e3ce38d825e1f7bcff
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-09-13 05:13:06 +0530

    vendor app added

---
+++ vendor/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-black text-white h-screen flex items-center justify-center">
+      Vendor App
+    </div>
+  );
+};
+
+export default App;
